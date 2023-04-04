4dd48a8e43ec7b05fd1711c83a300bec26afb115
Asahi Zhang <542792296@qq.com>
2023-04-04 20:09:30 +0800

    chore: add model sql

---
+++ alpgo-admin/src/main/resources/db/migration/sql/V1.0.20__addModel.sql
@@ -0,0 +1,4 @@
+
+INSERT INTO sys_dict_data
+(dict_sort, dict_value, dict_label, dict_type, css_class, list_class, is_default, status, create_by, create_time, update_by, update_time, remark)
+VALUES(1, 'revAnimated', 'revAnimated', 'stable_diffusion_model','', 'info', 'N', '0', 'admin', '2022-11-22 14:51:31', '', NULL, 'revAnimated');
