eeaacd0381192d606ad6860d10765ac2a1bc39d4
Asahi Zhang <542792296@qq.com>
2023-04-06 21:15:12 +0800

    feat: refine layout neo4j

--- alpgo-ui/src/components/Neo4jView/index.vue
+++ alpgo-ui/src/components/Neo4jView/index.vue
@@ -81,20 +81,38 @@ export default {
         edges: edges
       };
       var options = {
-        physics: {
-          enabled: false,
+        nodes: {
+          shape: "dot",
+          scaling: {
+            min: 10,
+            max: 30,
+            label: {
+              min: 8,
+              max: 30,
+              drawThreshold: 12,
+              maxVisible: 20,
+            },
+          },
+          font: {
+            size: 12,
+            face: "Tahoma",
+          },
+        },
+        edges: {
+          width: 0.15,
+          color: { inherit: "from" },
+          smooth: {
+            type: "continuous",
+          },
+        },
+        physics: false,
+        interaction: {
+          tooltipDelay: 200,
+          hideEdgesOnDrag: true,
+          hideEdgesOnZoom: true,
         },
         autoResize: true,
         layout: {
-        },
-        edges: {
-          physics: false,
-          smooth: true,
-          // arrows: { to: true },
-        },
-        interaction:{
-          hover:true,
-          hoverConnectedEdges: true
         },
         groups: {
           useDefaultGroups: true,
